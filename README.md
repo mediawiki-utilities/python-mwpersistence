4623042ab74988daa0e136c39b6d8d7ba4307f4a
halfak <aaron.halfaker@gmail.com>
2015-09-07 00:43:27 -0500

    Adds requirements and cleanups setup.py/README.md

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 This library provides a set of utilities for measuring content persistence and
 tracking authorship in MediaWiki revisions.
 
-* **Install:** ``pip install mwpersistence``
-* **Documentation:** https://pythonhosted.org/mwpersistence
+* **Install:** ``pip install mwpersistence`` (coming coon)
+* **Documentation:** https://pythonhosted.org/mwpersistence (coming soon)
 * **Repository:** https://github.com/mediawiki-utilities/mwpersistence
 * **License:** MIT
 
--- setup.py
+++ setup.py
@@ -6,26 +6,29 @@ from setuptools import find_packages, setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 def requirements(fname):
     for line in open(os.path.join(os.path.dirname(__file__), fname)):
         yield line.strip()
 
+
 setup(
-    name = "wikiclass",
-    version = "0.1.0",  # see wikiclass/__init__.py
-    author = "Aaron Halfaker / Morten Warncke-Wang",
-    author_email = "ahalfaker@wikimedia.org",
-    description = ("A library for performing automatic detection of assessment classes of Wikipedia articles."),
-    license = "MIT",
-    url = "https://github.com/halfak/wikiclass",
-    packages = find_packages(),
-    entry_points = {
+    name="mwpersistence",
+    version="0.0.1",  # see mwpersistence/__init__.py
+    author="Aaron Halfaker",
+    author_email="ahalfaker@wikimedia.org",
+    description="A set of utilities for measuring content persistence and " +
+                "tracking authorship in MediaWiki revisions.",
+    license="MIT",
+    url="https://github.com/mediawiki-utilities/python-mwpersistence",
+    packages=find_packages(),
+    entry_points={
         'console_scripts': [
-            'wikiclass=wikiclass.wikiclass:main'
+            'mwpersistence=mwpersistence.mwpersistence:main'
         ],
     },
-    long_description = read('README.md'),
-    install_requires = requirements('requirements.txt'),
+    long_description=read('README.md'),
+    install_requires=requirements('requirements.txt'),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Libraries :: Python Modules",
