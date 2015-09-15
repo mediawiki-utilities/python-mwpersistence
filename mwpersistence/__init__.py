fb0d5c12eea0d3aa7746ea3462c834bb5397e050
halfak <aaron.halfaker@gmail.com>
2015-09-15 18:14:06 -0500

    Adds utilities functions to main moduled

--- mwpersistence/__init__.py
+++ mwpersistence/__init__.py
@@ -1,4 +1,11 @@
 from .token import Token
 from .state import State, DiffState
+from .utilities.dump2diffs import dump2diffs
+from .utilities.diffs2persistence import diffs2persistence
+from .utilities.persistence2stats import persistence2stats
+from .utilities.dump2stats import dump2stats
 
 __version__ = "0.1.0"
+
+__all__ = [Token, State, DiffState, dump2diffs, diffs2persistence,
+           persistence2stats, dump2stats]
